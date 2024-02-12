10e0f8b648347863d367c6ef7874d8b44d061f23
herickkgb <jucefino9890@gmail.com>
2024-02-12 17:44:00 -0300

    primeiro commit aplicação biblioteca FRONTEND

---
+++ src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Categoria } from '../categoria.model';
+import { CategoriaService } from '../categoria-service.service';
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css'],
+})
+export class CategoriaReadComponent implements OnInit {
+  categorias: Categoria[] = [];
+  constructor(private service: CategoriaService) {}
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+  findAll() {
+    this.service.findAll().subscribe((resposta) => {
+      this.categorias = resposta;
+    });
+  }
+
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'acoes'];
+}
---
+++ src/environments/enviroments.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  baseUrl: 'https://bibliotecadelivros-production.up.railway.app/'
+};
