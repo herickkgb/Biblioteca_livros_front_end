d8a2ae992929267d4c839f4e27cfca0be9284aea
herickkgb <jucefino9890@gmail.com>
2024-02-12 21:44:58 -0300

    Metodo create e tratamento de erros

--- src/app/components/views/categoria/categoria-create/categoria-create.component.ts
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Categoria } from './../categoria.model';
+import { CategoriaService } from './../categoria-service.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-categoria-create',
   templateUrl: './categoria-create.component.html',
-  styleUrl: './categoria-create.component.css'
+  styleUrl: './categoria-create.component.css',
 })
-export class CategoriaCreateComponent {
+export class CategoriaCreateComponent implements OnInit {
+ categoria: Categoria = {
+nome : '',
+descricao : ''
+ }
+
+  constructor(private categoriaService: CategoriaService,private router: Router) {}
+  ngOnInit(): void {}
+  returnHome() {
+    this.router.navigate(['']);
+  }
+
+  create():void{
+    this.categoriaService.create(this.categoria)
+    .subscribe((resposta) =>{
+      this.categoriaService.mensagem('Categoria criada com sucesso!')
+      this.router.navigate(['categorias']);
+    },err =>{
+      for(let i =0; i < err.error.errors.length; i++){
+        this.categoriaService.mensagem(err.error.errors[i].message)
+      }
+    })
+  }
 
 }
--- src/app/components/views/categoria/categoria-service.service.ts
+++ src/app/components/views/categoria/categoria-service.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Categoria } from './categoria.model';
 import { environment } from '../../../../environments/enviroments';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +11,22 @@ import { environment } from '../../../../environments/enviroments';
 export class CategoriaService {
   baseUrl: string = environment.baseUrl;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient,private _snack:MatSnackBar) {}
 
   findAll(): Observable<Categoria[]> {
     const url = `${this.baseUrl}/categorias`;
     return this.http.get<Categoria[]>(url);
+  }
+
+  create(categotia: Categoria): Observable<Categoria> {
+    const url = `${this.baseUrl}/categorias`;
+    return this, this.http.post<Categoria>(url, categotia);
+  }
+  mensagem(str:String):void{
+    this._snack.open(`${str}`, 'OK',{
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 5000
+    })
   }
 }
