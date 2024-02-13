66a1ca1dacad83c2633f9e4bd818b8df4477adba
herickkgb <jucefino9890@gmail.com>
2024-02-13 17:31:20 -0300

    Finalisando o Update

--- src/app/components/views/categoria/categoria-service.service.ts
+++ src/app/components/views/categoria/categoria-service.service.ts
@@ -15,23 +15,27 @@ export class CategoriaService {
   constructor(private http: HttpClient, private _snack: MatSnackBar) { }
 
   findAll():Observable<Categoria[]> {
-    const url = `${this.baseUrl}/categorias`
+    const url = `${this.baseUrl}categorias`
     return this.http.get<Categoria[]>(url)
   }
 
   findById(id: String): Observable<Categoria> {
-    const url = `${this.baseUrl}/categorias/${id}`
+    const url = `${this.baseUrl}categorias/${id}`
     return this.http.get<Categoria>(url)
   }
 
   create(categoria: Categoria): Observable<Categoria>{
-    const url = `${this.baseUrl}/categorias`
+    const url = `${this.baseUrl}categorias`
     return this.http.post<Categoria>(url, categoria);
   }
 
   delete(id: String):Observable<void> {
-    const url = `${this.baseUrl}/categorias/${id}`
+    const url = `${this.baseUrl}categorias/${id}`
     return this.http.delete<void>(url)
+  }
+  update(categoria: Categoria):Observable<void> {
+    const url = `${this.baseUrl}categorias/${categoria.id}`
+    return this.http.put<void>(url, categoria)
   }
 
   mensagem(str: String): void {
--- src/app/components/views/categoria/categoria-update/categoria-update.component.ts
+++ src/app/components/views/categoria/categoria-update/categoria-update.component.ts
@@ -1,31 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { CategoriaService } from '../categoria-service.service';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Categoria } from '../categoria.model';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Categoria } from "../categoria.model";
+import { CategoriaService } from "../categoria-service.service";
 
 @Component({
-  selector: 'app-categoria-update',
-  templateUrl: './categoria-update.component.html',
-  styleUrl: './categoria-update.component.css'
+  selector: "app-categoria-update",
+  templateUrl: "./categoria-update.component.html",
+  styleUrls: ["./categoria-update.component.css"],
 })
-export class CategoriaUpdateComponent implements OnInit{
-  constructor(private service: CategoriaService, private route: ActivatedRoute, private router: Router) { }
-
+export class CategoriaUpdateComponent implements OnInit {
   categoria: Categoria = {
     id: '',
     nome: '',
-    descricao: ''
+    descricao: '',
+  };
+
+  constructor(
+    private service: CategoriaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.categoria.id = this.route.snapshot.paramMap.get('id')!;
+    this.findById();
   }
 
   findById(): void {
     this.service.findById(this.categoria.id!).subscribe((resposta) => {
-      this.categoria.nome = resposta.nome
-      this.categoria.descricao = resposta.descricao
-    })
+      this.categoria.nome = resposta.nome;
+      this.categoria.descricao = resposta.descricao;
+    });
   }
 
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  update(): void {
+    this.service.update(this.categoria).subscribe((resposta) => {
+      this.router.navigate(['categorias']);
+      this.service.mensagem("Categoria atualizada com sucesso");
+    }, err => {
+      this.service.mensagem('Validar se todos os campos estão preenchidos corretamente!')
+    });
+  }
+
+  cancel(): void {
+    this.router.navigate(['categorias'])
   }
 
 }
